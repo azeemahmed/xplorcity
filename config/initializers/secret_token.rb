703438750d218d2dea1da40b64a595c60c6b94a7
Azeem Ahmed <azeem498@yahoo.co.in>
2013-03-07 17:58:38 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xplorcity::Application.config.secret_token = '851e606df975f6f59035cd406e2e8b10d8891d86864423696f2fbae21bf364c5e625d207ec55d0f1e43335da061a1f153a9765990f07a23987864e06377a2b28'
